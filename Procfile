8085c06a195c22e0b57a770487d9f7c2e7b9053e
Jamie Gullbrand <jamiegullbrand@gmail.com>
2019-02-14 15:25:03 -0500

    prep for heroku deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn oscars_project.wsgi
--- oscars_project/settings.py
+++ oscars_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,4 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
