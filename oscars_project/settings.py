829ca4480513f2d50d18b1202de74ccd11a1514b
Jamie Gullbrand <jamiegullbrand@gmail.com>
2019-02-20 13:20:52 -0500

    changed key + updated env variable

--- oscars_project/settings.py
+++ oscars_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!hl5_3bogkt%udu39#$e_g0f9m_#f0_44e*^r@vq-=6t%(g-gx'
+SECRET_KEY = os.environ.get('SECRET_KEY2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
